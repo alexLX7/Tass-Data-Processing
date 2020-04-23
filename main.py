d8309e2ea9633f581caead1837c52cef17bcbaad
alexLAP7 <dat1chilldevm8@yandex.ru>
2020-04-23 03:38:03 +0300

    Added the first step of processing

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from extractor import Extractor
+import time
 
 
 def main():
 
-    print('processing...')
+    start_time = time.time()
+    path = 'data/test_0.pickle'  # be aware of 'unix/path'
+
+    print('extracting...')
+    extractor = Extractor(path)
+    extractor.run()
+
+    print('\nDONE.')
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
